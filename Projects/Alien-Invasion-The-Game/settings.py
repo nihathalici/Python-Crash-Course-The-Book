a720a9fb07c075aa699386d337c2363505443775
Nihat Halici <nihat.halici@gmail.com>
2021-05-18 12:03:34 +0200

    Update settings.py

--- Projects/Alien-Invasion-The-Game/settings.py
+++ Projects/Alien-Invasion-The-Game/settings.py
@@ -4,7 +4,16 @@ class Settings:
     def __init__(self):
         """Initalize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 300
+        self.screen_height = 200
         self.bg_color = (230, 230, 230)
-        
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
