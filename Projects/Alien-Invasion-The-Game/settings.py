162731c6d9f4a88131fa4d0dbecd0f0cfa0e4b61
Nihat Halici <nihat.halici@gmail.com>
2021-05-23 07:11:16 -0700

    Update settings.py

--- Projects/Alien-Invasion-The-Game/settings.py
+++ Projects/Alien-Invasion-The-Game/settings.py
@@ -9,24 +9,33 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
- 
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        
+        self.initialize_dynamic_settings()sac
+
 def initialize_dynamic_settings(self):
-    pass
+    """Initalize settings that change throughout the game."""
+    self.ship_speed = 1.5
+    self.bullet_speed = 3.0
+    self.alien_speed = 1.0
+
+    # fleet_direction of 1 represents right; -1 represents left.
+    self.fleet_direction = 1
 
 def increase_speed(self):
-    pass
+    """Increase speed settings."""
+    self.ship_speed *= self.speedup_scale
+    self.bullet_speed *= self.speedup_scale
+    self.alien_speed *= self.speedup_scale
