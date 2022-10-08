823039fb4a93bcfbab9f792c41f3951d94b673c6
Nihat Halici <nihat.halici@gmail.com>
2022-10-08 04:17:03 +0200

    Add files via upload

--- Projects/Alien-Invasion-The-Game/2nd-Loop/settings.py
+++ Projects/Alien-Invasion-The-Game/2nd-Loop/settings.py
@@ -3,8 +3,20 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+
+        # Ship settings
+        self.ship_speed = 1.5
+        
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230, 230, 230)
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        
 
