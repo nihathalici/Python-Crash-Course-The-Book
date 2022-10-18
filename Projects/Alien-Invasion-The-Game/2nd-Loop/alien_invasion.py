041746a40f9512b0cd71e44744844d80d983ed08
Nihat Halici <nihat.halici@gmail.com>
2022-10-18 06:57:02 +0200

    Add files via upload

--- Projects/Alien-Invasion-The-Game/2nd-Loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/2nd-Loop/alien_invasion.py
@@ -66,6 +66,9 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Reset the game statistics.
+            self.settings.initialize_dynamic_settings()
+
+            # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -126,6 +129,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
