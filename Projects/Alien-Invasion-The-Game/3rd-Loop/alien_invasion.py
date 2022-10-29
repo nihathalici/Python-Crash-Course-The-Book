2bed68cf504ff733206e67001d919220846db291
Nihat Halici <nihat.halici@gmail.com>
2022-10-29 09:34:12 +0200

    Add files via upload

--- Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 
+from settings import Settings
+
 import pygame
 
 class AlienInvasion:
@@ -8,9 +10,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
+
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -19,6 +24,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
