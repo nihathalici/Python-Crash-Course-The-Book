abb1a8dc61217c397b71446ac0b431b9cef334df
Nihat Halici <nihat.halici@gmail.com>
2022-11-14 07:30:46 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -31,6 +32,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Make the Play button.
+        self.play_button = Button(self, "Play")
 
     
     def run_game(self):
@@ -206,6 +210,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         
         pygame.display.flip()
 
