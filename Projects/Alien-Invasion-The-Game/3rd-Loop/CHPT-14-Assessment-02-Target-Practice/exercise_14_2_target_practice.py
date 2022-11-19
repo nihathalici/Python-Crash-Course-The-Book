7aa2e4c3be81faf316c81eaec75604c64e7869e9
Nihat Halici <nihat.halici@gmail.com>
2022-11-19 15:54:07 +0100

    Update exercise_14_2_target_practice.py

--- Projects/Alien-Invasion-The-Game/3rd-Loop/CHPT-14-Assessment-02-Target-Practice/exercise_14_2_target_practice.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/CHPT-14-Assessment-02-Target-Practice/exercise_14_2_target_practice.py
@@ -33,33 +33,90 @@ class Screen:
         self.play_button = Button(self)
     
     def run_game(self):
-        pass
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.ship.ship_move_up = True
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.ship_move_down = True
+                    elif event.key == pygame.K_SPACE:
+                        new_bullet = Bullet(self)
+                        self.bullets.add(new_bullet)
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.ship.ship_move_up = False
+                    elif event.key == pygame.K_DOWN:
+                        self.ship.ship_move_down = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    clicked_position = pygame.mouse.get_pos()
+                    button_clicked = self.play_button.rect.collidepoint(
+                        clicked_position)
+                    if not self.game_active and button_clicked:
+                        self.game_active = True
+
+            if self.game_active:
+                self.ship_update()
+                self.bullets.update()
+                self.box.update()
+                self._check_bullet_hit()
+                self._check_box_edges()
+
+            self.screen.fill(self.screen_color)
+            self.screen.blit(self.ship.image, self.ship.rect)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+            self.box.draw_box()
+            if not self.game_active:
+                self.play_button.draw_button()
+            pygame.display.flip()  
 
     def ship_update(self):
-        pass
+        if self.ship.ship_move_up and self.ship.rect.y > 0:
+            self.ship.rect.y -= self.ship.ship_speed
+        if self.ship.ship_move_down and self.ship.rect.bottom < self.screen_rect.bottom:
+            self.ship.rect.y += self.ship.ship_speed 
 
     def _check_box_edges(self):
-        pass
+        if self.box.rect.bottom >= self.screen_rect.bottom or self.box.rect.top <= 0:
+            self.box.move_direction *= -1
 
     def _check_bullet_hit(self):
-        pass
+        collision = pygame.sprite.spritecollide(self.box, self.bullets, True)
+        for bullet in self.bullets.copy():
+            if bullet.rect.left > self.screen_rect.right:
+                self.bullets.remove(bullet)
+                self.missed += 1
+                self._check_game_over() 
 
     def _check_game_over(self):
-        pass
-
-
-
-
-
+        if self.missed == 3:
+            self.missed = 0
+            self.game_active = False
 
 
 class Ship:
     def __init__(self, screen_rect):
-        pass
+        self.screen_rect = screen_rect
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.midleft = self.screen_rect.midleft
+        self.ship_speed = 1
+        self.ship_move_up = False
+        self.ship_move_down = False
 
 class Bullet(Sprite):
     def __init__(self, screen):
-        pass
+        super().__init__()
+        self.screen = screen
+        self.bullet_speed = 1.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.rect = pygame.Rect(0, 0, self.bullet_width, self.bullet_height)
+        self.rect.midright = self.screen.ship.rect.midright
 
     def update(self):
         pass
