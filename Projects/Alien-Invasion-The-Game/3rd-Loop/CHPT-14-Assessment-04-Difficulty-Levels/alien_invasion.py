86f9cc73a785a8944edcbcb1ff7a423710470f1c
Nihat Halici <nihat.halici@gmail.com>
2022-11-22 05:56:29 +0100

    Add files via upload

---
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/CHPT-14-Assessment-04-Difficulty-Levels/alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+from time import sleep
+
+import pygame
+from settings import Settings
+from game_stats import GameStats
+from button import Button
+from ship import Ship
+from bullet import Bullet
+from alien import Alien
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pass
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        pass
+
+
+
+if __name__ == "__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
