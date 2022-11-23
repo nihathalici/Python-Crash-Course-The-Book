b314518bdfa2b873d3539d5ce86e1c617783f960
Nihat Halici <nihat.halici@gmail.com>
2022-11-24 00:51:33 +0100

    Update alien_invasion.py

--- Projects/Alien-Invasion-The-Game/3rd-Loop/CHPT-14-Assessment-04-Difficulty-Levels/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/CHPT-14-Assessment-04-Difficulty-Levels/alien_invasion.py
@@ -14,7 +14,34 @@ class AlienInvasion:
 
     def __init__(self):
         """Initialize the game, and create game resources."""
-        pass
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        
+        # Running the Game in Fullscreen Mode
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption(self.settings.caption)
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
+
+        # Make the play button.
+        self.play_button_easy = Button(self, "Easy", (350, 400))
+        self.play_button_normal = Button(self, "Normal", (600, 400))
+        self.play_button_hard = Button(self, "Hard", (850, 400))
+        self.play_buttons = []
+        self.play_buttons.append(self.play_button_easy)
+        self.play_buttons.append(self.play_button_normal)
+        self.play_buttons.append(self.play_button_hard)
 
     def run_game(self):
         """Start the main loop for the game."""
