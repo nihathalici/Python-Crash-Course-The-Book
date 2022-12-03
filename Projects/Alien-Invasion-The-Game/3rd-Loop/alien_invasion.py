43d0ee9a2291a38c67a805cc86de65eabb503866
Nihat Halici <nihat.halici@gmail.com>
2022-12-03 16:59:19 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -24,8 +25,10 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics,
+        # and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -232,6 +235,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
