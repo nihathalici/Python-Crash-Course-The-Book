ff87381f965f39efae3777396e08293a1c077716
Nihat Halici <nihat.halici@gmail.com>
2022-12-06 09:16:40 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/3rd-Loop/alien_invasion.py
@@ -78,6 +78,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -141,13 +143,18 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
 
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
