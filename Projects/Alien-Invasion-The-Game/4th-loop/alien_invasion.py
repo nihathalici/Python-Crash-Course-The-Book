875512087bc6524e9ab868369b625df74e29d494
Nihat Halici <nihat.halici@gmail.com>
2022-12-08 15:30:03 +0100

    Update alien_invasion.py

--- Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
@@ -21,17 +21,21 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-            
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            
 
 if __name__ == '__main__':
     ai = AlienInvasion()
