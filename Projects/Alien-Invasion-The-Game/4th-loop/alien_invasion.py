85da26746d5ae2aab656abb4e022d0351d943f6e
Nihat Halici <nihat.halici@gmail.com>
2022-12-09 13:02:32 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
@@ -22,19 +22,33 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
+            self._update_screen()
             
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-    
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        pygame.display.flip()
             
 
 if __name__ == '__main__':
--- Projects/Alien-Invasion-The-Game/4th-loop/settings.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/settings.py
@@ -8,4 +8,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Ship settings
+        self.ship_speed = 1.5
+
 
