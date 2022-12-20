aa09af056a471322fbc76d89cf20c0ef561c9d18
Nihat Halici <nihat.halici@gmail.com>
2022-12-20 07:43:55 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/4th-loop/game_stats.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/game_stats.py
@@ -5,8 +5,8 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+        # Start game in an inactive state.
+        self.game_active = False
 
     
     def reset_stats(self):
