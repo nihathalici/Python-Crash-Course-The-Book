356584b765e0d56682e5caae24fc6bf663c19969
Nihat Halici <nihat.halici@gmail.com>
2022-12-31 00:46:12 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/4th-loop/button.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/button.py
@@ -1,5 +1,6 @@
 import pygame.font
 
+
 class Button:
     def __init__(self, ai_game, msg):
         """Initialize button attributes."""
@@ -15,24 +16,17 @@ class Button:
         # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
-        
+
         # The button message needs to be prepped only once.
         self._prep_msg(msg)
-    
+
     def _prep_msg(self, msg):
         """Turn msg into a rendered image and center text on the button."""
-        self.msg_image = self.font.render(msg, True, self.text_color,
-                self.button_color)
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
-    
+
     def draw_button(self):
         # Draw blank button and then draw message.
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
-
-
-
-
-
-
--- Projects/Alien-Invasion-The-Game/4th-loop/game_stats.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/game_stats.py
@@ -5,13 +5,10 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        # Start game in an inactive state.
+
+        # Start game in an active state.
         self.game_active = False
 
-    
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
-
-
-
