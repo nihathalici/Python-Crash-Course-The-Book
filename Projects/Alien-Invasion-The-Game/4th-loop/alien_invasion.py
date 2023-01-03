2aabcd0d6c334f7170a2a398139e01cf4ddc8d3a
Nihat Halici <nihat.halici@gmail.com>
2023-01-03 01:26:33 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/alien_invasion.py
@@ -120,11 +120,11 @@ class AlienInvasion:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
-
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
