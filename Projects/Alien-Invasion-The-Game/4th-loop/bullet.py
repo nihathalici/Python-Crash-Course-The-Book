fedb214314959c7b4e92e3ef927ef5526198e19a
Nihat Halici <nihat.halici@gmail.com>
2023-01-10 20:56:47 +0100

    Add files via upload

--- Projects/Alien-Invasion-The-Game/4th-loop/bullet.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/bullet.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
-
+ 
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
 
@@ -15,20 +15,17 @@ class Bullet(Sprite):
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
             self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
-
+        
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
-    
+
     def update(self):
         """Move the bullet up the screen."""
         # Update the decimal position of the bullet.
         self.y -= self.settings.bullet_speed
         # Update the rect position.
         self.rect.y = self.y
-    
+
     def draw_bullet(self):
+        """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
-
-
-
-
--- Projects/Alien-Invasion-The-Game/4th-loop/settings.py
+++ Projects/Alien-Invasion-The-Game/4th-loop/settings.py
@@ -9,6 +9,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
+        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings
@@ -23,6 +24,9 @@ class Settings:
         # How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -34,8 +38,13 @@ class Settings:
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
